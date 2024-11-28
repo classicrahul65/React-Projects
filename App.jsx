957ec47e50f218598e22b8ddfc989f8c6d080e99
Rahul Singh <rahul118553@gmail.com>
2024-11-28 17:27:21 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Form from "./form";
+import { nanoid } from "nanoid";
+import Items from "./Items";
+import { ToastContainer, toast } from "react-toastify";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    list = JSON.parse(localStorage.getItem("list"));
+  } else {
+    list = [];
+  }
+  return list;
+};
+
+const setLocalStorage = (items) => {
+  localStorage.setItem("list", JSON.stringify(items));
+};
+
+const App = () => {
+  const [items, setItems] = useState(getLocalStorage());
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success("added");
+  };
+
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success("removed");
+  };
+
+  return (
+    <section className="section-center">
+      <ToastContainer position="top-center" />
+      <Form addItem={addItem} />
+      <Items items={items} removeItem={removeItem} />
+    </section>
+  );
+};
+
+export default App;
