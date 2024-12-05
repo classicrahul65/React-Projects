a54dacae51026e40cd01256bf73a77f3ee08c010
Rahul Singh <rahul118553@gmail.com>
2024-12-05 12:16:19 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,52 +1,14 @@
-import { useState } from "react";
-import Form from "./form";
-import { nanoid } from "nanoid";
-import Items from "./Items";
-import { ToastContainer, toast } from "react-toastify";
-
-const getLocalStorage = () => {
-  let list = localStorage.getItem("list");
-  if (list) {
-    list = JSON.parse(localStorage.getItem("list"));
-  } else {
-    list = [];
-  }
-  return list;
-};
-
-const setLocalStorage = (items) => {
-  localStorage.setItem("list", JSON.stringify(items));
-};
+import Home from "./Home";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
 
 const App = () => {
-  const [items, setItems] = useState(getLocalStorage());
-
-  const addItem = (itemName) => {
-    const newItem = {
-      name: itemName,
-      completed: false,
-      id: nanoid(),
-    };
-    const newItems = [...items, newItem];
-    setItems(newItems);
-    setLocalStorage(newItems);
-    toast.success("added");
-  };
-
-  const removeItem = (itemId) => {
-    const newItems = items.filter((item) => item.id !== itemId);
-    setItems(newItems);
-    setLocalStorage(newItems);
-    toast.success("removed");
-  };
-
   return (
-    <section className="section-center">
-      <ToastContainer position="top-center" />
-      <Form addItem={addItem} />
-      <Items items={items} removeItem={removeItem} />
-    </section>
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
   );
 };
-
 export default App;
